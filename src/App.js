6e69dcdc950860983e4cc7a8a09b951761c77629
Johnson <zxmagichacker@gmail.com>
2021-05-04 21:40:38 +0800

    feat: App组件中注册路由

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import logo from './logo.svg';
 import './App.less';
-
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/login';
+import HomePage from './pages/homePage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="app_wrapper">
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/homepage" component={HomePage} />
+      </Switch>
     </div>
   );
 }
