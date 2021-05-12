c0be936b12ff0fa2a523ba8107c550e0ac23e054
zhangxuan6 <zhangxuan6@aixuexi.com>
2021-05-12 17:19:38 +0800

    feat: 项目配置多语言

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
 import './index.less';
+import zhCN from 'antd/lib/locale/zh_CN';
+import 'moment/locale/zh-cn';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ConfigProvider locale={zhCN}>
+        <App />
+      </ConfigProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
