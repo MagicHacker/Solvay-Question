820409dc9b7bcb26927fd1944da435eed3f90a0b
zhangxuan6 <zhangxuan6@aixuexi.com>
2021-05-12 15:51:32 +0800

    feat: 构建学员纠错

--- src/pages/studentError/index.js
+++ src/pages/studentError/index.js
@@ -1,7 +1,43 @@
 import React, { Component } from 'react';
-
+import { Form, Input, Space, Button, DatePicker, Tabs, Card } from 'antd';
+import './index.less';
+const { RangePicker } = DatePicker;
+const { TabPane } = Tabs;
 export default class StudentError extends Component {
   render() {
-    return <div>StudentError</div>;
+    return (
+      <div className="student_error_wrapper">
+        <div className="student_error_title">学员纠错</div>
+        <div className="student_error_search">
+          <Form layout="inline">
+            <Form.Item label="时间筛选:">
+              <RangePicker showTime />
+            </Form.Item>
+            <Form.Item label="试题名称:">
+              <Input />
+            </Form.Item>
+            <Form.Item label="试题编码:">
+              <Input />
+            </Form.Item>
+            <Form.Item>
+              <Space>
+                <Button type="primary" htmlType="submit">
+                  查询
+                </Button>
+                <Button>重置</Button>
+              </Space>
+            </Form.Item>
+          </Form>
+        </div>
+        <Tabs type="card" className="student_error_tabs">
+          <TabPane tab="已解决" key="1">
+            <Card title="题目">已解决</Card>
+          </TabPane>
+          <TabPane tab="未解决" key="2">
+            <Card title="题目">未解决</Card>
+          </TabPane>
+        </Tabs>
+      </div>
+    );
   }
 }
