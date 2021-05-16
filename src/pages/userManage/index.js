f8923c220ed86debb219edb52ce6279dcbb68704
Johnson <zxmagichacker@gmail.com>
2021-05-16 11:11:00 +0800

    feat: 用户管理绑定真实事件和数据

--- src/pages/userManage/index.js
+++ src/pages/userManage/index.js
@@ -4,11 +4,18 @@ import './index.less';
 export default class UserManage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      registerTime: '',
+      loginTime: '',
+      userName: '',
+      phone: '',
+      nickName: '',
+      userIdentity: 0,
+    };
     this.dataSource = [
       {
         key: '1',
-        name: '胡彦斌',
+        userName: '胡彦斌',
         phone: 32,
         registerTime: '2019-5-10',
         id: 45678,
@@ -20,7 +27,7 @@ export default class UserManage extends Component {
       },
       {
         key: '2',
-        name: '胡彦祖',
+        userName: '胡彦祖',
         phone: 42,
         registerTime: '2019-5-10',
         id: 45678,
@@ -41,8 +48,8 @@ export default class UserManage extends Component {
       },
       {
         title: '姓名',
-        dataIndex: 'name',
-        key: 'name',
+        dataIndex: 'userName',
+        key: 'userName',
         align: 'center',
       },
       {
@@ -106,39 +113,88 @@ export default class UserManage extends Component {
       },
     ];
   }
+  changeRegisterTime = (date, dateString) => {
+    this.setState({
+      registerTime: dateString,
+    });
+  };
+  changeLoginTime = (date, dateString) => {
+    this.setState({
+      loginTime: dateString,
+    });
+  };
+  changeUserName = (e) => {
+    const { target } = e;
+    this.setState({
+      userName: target.value,
+    });
+  };
+  changeNickName = (e) => {
+    const { target } = e;
+    this.setState({
+      nickName: target.value,
+    });
+  };
+  changePhone = (e) => {
+    const { target } = e;
+    this.setState({
+      phone: target.value,
+    });
+  };
+  changeUserIdentity = (e) => {
+    const { target } = e;
+    this.setState({
+      userIdentity: target.value,
+    });
+  };
+  search = () => {
+    const { userName, phone, nickName, userIdentity, registerTime, loginTime } = this.state;
+    console.warn('输出的值', userName, phone, nickName, userIdentity, registerTime, loginTime);
+  };
+  reset = () => {
+    this.setState({
+      userName: '',
+      phone: '',
+      nickName: '',
+      userIdentity: 0,
+      registerTime: '',
+      loginTime: '',
+    });
+  };
   render() {
+    const { userName, phone, nickName, userIdentity } = this.state;
     return (
       <div className="user_manage_wrapper">
         <div className="user_manage_title">用户管理</div>
         <div className="user_manage_search">
           <Form layout="inline">
             <Form.Item label="注册时间:" labelCol={{ span: 6 }} wrapperCol={{ span: 16 }} style={{ marginTop: 10 }}>
-              <DatePicker showTime />
+              <DatePicker showTime onChange={this.changeRegisterTime} />
             </Form.Item>
             <Form.Item label="登录时间:" labelCol={{ span: 6 }} wrapperCol={{ span: 16 }} style={{ marginTop: 10 }}>
-              <DatePicker showTime />
+              <DatePicker showTime onChange={this.changeLoginTime} />
             </Form.Item>
             <Form.Item label="姓名:" labelCol={{ span: 6 }} wrapperCol={{ span: 16 }} style={{ marginTop: 10 }}>
-              <Input />
+              <Input placeholder="请输入姓名" value={userName} onChange={this.changeUserName} />
             </Form.Item>
             <Form.Item label="昵称:" labelCol={{ span: 6 }} wrapperCol={{ span: 16 }} style={{ marginTop: 10 }}>
-              <Input />
+              <Input placeholder="请输入昵称" value={nickName} onChange={this.changeNickName} />
             </Form.Item>
             <Form.Item label="手机号:" labelCol={{ span: 6 }} wrapperCol={{ span: 16 }} style={{ marginTop: 10 }}>
-              <Input />
+              <Input placeholder="请输入手机号" value={phone} onChange={this.changePhone} />
             </Form.Item>
             <Form.Item label="用户身份:" style={{ marginTop: 10 }} labelCol={{ span: 7 }} wrapperCol={{ span: 17 }}>
-              <Radio.Group>
-                <Radio value="">已报名</Radio>
-                <Radio value="">未报名</Radio>
+              <Radio.Group value={userIdentity} onChange={this.changeUserIdentity}>
+                <Radio value={1}>已报名</Radio>
+                <Radio value={0}>未报名</Radio>
               </Radio.Group>
             </Form.Item>
             <Form.Item style={{ marginTop: 10 }} wrapperCol={{ span: 24 }}>
               <Space>
-                <Button type="primary" htmlType="submit">
+                <Button type="primary" htmlType="submit" onClick={this.search}>
                   查询
                 </Button>
-                <Button>重置</Button>
+                <Button onClick={this.reset}>重置</Button>
               </Space>
             </Form.Item>
           </Form>
