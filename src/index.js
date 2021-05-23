0e3081cba16788c2a58be3b086e8a385475d5c0c
Johnson <zxmagichacker@gmail.com>
2021-05-23 17:35:38 +0800

    feat: 使用BrowserRouter替换hashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
 import './index.less';
 import zhCN from 'antd/lib/locale/zh_CN';
